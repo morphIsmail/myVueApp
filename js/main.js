d633bfc492ada49504fc772781a218d03c1d8d40
morphIsmail <ismail_2016@mail.ru>
2021-06-07 16:04:09 +0300

    Вторая версия

--- js/main.js
+++ js/main.js
@@ -171,27 +171,3 @@ let app = new Vue({
     }
   }
 })
-
-let audio = new Audio('audio/soundtrack.mp3')
-let audio_btn = document.querySelector('.btn__sound')
-let audio_icon = document.querySelector('.btn__sound i')
-
-audio.muted = true;
-audio.autoplay = true;
-audio.volume = 0.2
-
-audio.addEventListener('loadmetadata', function() {
-  audio.currentTime = 0 + Math.random() * (audio.duration + 1 - 0)
-})
-
-audio_btn.addEventListener('click', function() {
-  if(audio.muted) {
-    audio.muted= false
-    audio_icon.classList.add('fa-volume-up')
-    audio_icon.classList.remove('fa-volume-off')
-  } else if(!audio.muted) {
-    audio.muted= true
-    audio_icon.classList.add('fa-volume-off')
-    audio_icon.classList.remove('fa-volume-up')
-  }
-})
